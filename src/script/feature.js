53b5b84983ccb3076450110d7de53e2db9f21d8a
lovindy <seangdy085@gmail.com>
2024-04-20 14:54:15 +0700

    feature : feature-page development

---
+++ src/script/feature.js
@@ -0,0 +1,28 @@
+const feature = document.querySelector(".feature-layout");
+fetch("../components/feature.html")
+  .then((res) => res.text())
+  .then((data) => {
+    feature.innerHTML = data;
+
+    // get button element
+    const filterBtn = document.querySelector("#filter-btn");
+    const release = document.querySelector("#release");
+
+    // get item element
+    const filterItem = document.querySelector("#filter");
+    const hideText = document.querySelector("#hide");
+    const showText = document.querySelector("#show");
+
+    // function action
+    filterBtn.addEventListener('click', () => {
+      filterItem.classList.toggle("hidden");
+      if (filterItem.classList.contains("hidden")) {
+        filterBtn.innerHTML = '<span class="cursor-pointer">Show Filters</span><img class="size-[20px] cursor-pointer" src="../images/home-icon/newspaper.png" alt="">';
+      } else {
+        filterBtn.innerHTML = '<span class="cursor-pointer">Hide Filters</span><img class="size-[20px] cursor-pointer" src="../images/home-icon/newspaper.png" alt="">';
+      }
+    });
+
+
+  })
+  .catch((error) => console.error("Error fetching included file:", error));
