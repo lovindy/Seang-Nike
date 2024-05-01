54b8b51cec4e4bdc69b5abff95a1fa1b9adadd81
lovindy <seangdy085@gmail.com>
2024-05-01 10:23:35 +0700

    feature : initial file of kid page.

--- src/script/kids.js
+++ src/script/kids.js
@@ -1,9 +1,9 @@
-const kidContent = document.querySelector(".kids-content-layout");
-fetch("../components/footer.html")
+const kidContent = document.querySelector(".kid-content-layout");
+fetch("../components/kid.html")
   .then((res) => res.text())
   .then((data) => {
     kidContent.innerHTML = data;
-
+    
 
   })
   .catch((error) => console.error("Error fetching included file:", error));
