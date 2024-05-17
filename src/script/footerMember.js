c9b522fb4cbde489fdd4eaba498cf9638f1123d9
lovindy <seangdy085@gmail.com>
2024-05-17 20:26:40 +0700

    fix error.

--- src/script/footerMember.js
+++ src/script/footerMember.js
@@ -1,16 +1,16 @@
 const footerMember = () => {
-    return `
+  return `
     <section class="w-full max-w-[1600px] mx-auto pt-20 pb-10">
     <h1 class="text-[26px] px-4">Frequently Asked Questions</h1>
     <div class="transition">
       <!-- header -->
       <div
-        class="accordion-header pt-20 rotate-text-1 cursor-pointer transition flex px-5 items-center justify-between h-12">
+        class="accordion-header-10 pt-20 rotate-text-10 cursor-pointer transition flex px-5 items-center justify-between h-12">
         <h3 class="text-[20px]">What is Nike Membership?</h3>
-        <img class="rotate-icon-1 size-[20px] rotate-360" src="../images/home-icon/down-arrow.png" alt="" />
+        <img class="rotate-icon-10 size-[20px] rotate-360" src="../images/home-icon/down-arrow.png" alt="" />
       </div>
       <!-- Content -->
-      <div class="accordion-content px-5 pt-0 overflow-hidden max-h-0">
+      <div class="accordion-content-10 px-5 pt-0 overflow-hidden max-h-0">
         <ul>
           <p class="py-10 text-[18px]">
             Nike Membership is access to the very best of Nike through any of our
@@ -28,12 +28,12 @@ const footerMember = () => {
     <div class="transition">
       <!-- header -->
       <div
-        class="accordion-header pt-10 rotate-text-1 cursor-pointer transition flex px-5 items-center justify-between h-12">
+        class="accordion-header-10 pt-10 rotate-text-10 cursor-pointer transition flex px-5 items-center justify-between h-12">
         <h3 class="text-[20px]">Am I a Nike Member?</h3>
-        <img class="rotate-icon-1 size-[20px] rotate-360" src="../images/home-icon/down-arrow.png" alt="" />
+        <img class="rotate-icon-10 size-[20px] rotate-360" src="../images/home-icon/down-arrow.png" alt="" />
       </div>
       <!-- Content -->
-      <div class="accordion-content px-5 pt-0 overflow-hidden max-h-0">
+      <div class="accordion-content-10 px-5 pt-0 overflow-hidden max-h-0">
         <ul>
           <p class="py-10 text-[18px]">
             Maybe! If you’ve ever logged into a Nike app, then yes, yes you are.
@@ -49,12 +49,12 @@ const footerMember = () => {
     <div class="transition">
       <!-- header -->
       <div
-        class="accordion-header pt-10 rotate-text-1 cursor-pointer transition flex px-5 items-center justify-between h-12">
+        class="accordion-header-10 pt-10 rotate-text-10 cursor-pointer transition flex px-5 items-center justify-between h-12">
         <h3 class="text-[20px]">Is Nike Membership free?</h3>
-        <img class="rotate-icon-1 size-[20px] rotate-360" src="../images/home-icon/down-arrow.png" alt="" />
+        <img class="rotate-icon-10 size-[20px] rotate-360" src="../images/home-icon/down-arrow.png" alt="" />
       </div>
       <!-- Content -->
-      <div class="accordion-content px-5 pt-0 overflow-hidden max-h-0">
+      <div class="accordion-content-10 px-5 pt-0 overflow-hidden max-h-0">
         <ul>
           <p class="py-10 text-[18px]">
             100% yes. Members enjoy all the benefits of Nike Membership at no cost, because once you’re in, we got you.
@@ -71,9 +71,66 @@ const footerMember = () => {
 }
 
 class FooterMember extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = footerMember();
+  connectedCallback() {
+    this.innerHTML = footerMember();
+    // dropdown
+    const accordionHeader = document.querySelectorAll(".accordion-header-10");
+    accordionHeader.forEach((header) => {
+      header.addEventListener("click", function () {
+        const accordionContent =
+          header.parentElement.querySelector(".accordion-content-10");
+        let accordionMaxHeight = accordionContent.style.maxHeight;
+
+        // Condition handling
+        if (accordionMaxHeight == "0px" || accordionMaxHeight.length == 0) {
+          accordionContent.style.maxHeight = `${accordionContent.scrollHeight + 32
+            }px`; // Fix interpolation
+        } else {
+          accordionContent.style.maxHeight = "0px"; // Add quotes
+        }
+      });
+    });
+
+    const rotateIcons = document.querySelectorAll(".rotate-icon-10");
+    const rotateTexts = document.querySelectorAll(".rotate-text-10");
+
+    rotateTexts.forEach((rotateText, index) => {
+      rotateText.addEventListener("click", () => {
+        rotateIcons[index].classList.toggle("rotate-180");
+        rotateIcons[index].classList.toggle("rotate-360");
+      });
+    });
+
+    // shop carousel
+    const shopPrev1 = document.querySelector("#prev-shop-1");
+    const shopNext1 = document.querySelector("#next-shop-1");
+    const shopImage1 = document.querySelector("#shop-slider-1");
+    const shopSlider1 = document.querySelectorAll("#card-shop-1");
+
+    let currentIndexShop1 = 0;
+
+    function nextShop1() {
+      if (currentIndexShop1 < 2) {
+        currentIndexShop1 = (currentIndexShop1 + 1) % shopSlider1.length;
+        updateShop1();
+      }
     }
+
+    function prevShop1() {
+      if (currentIndexShop1 > 0) {
+        currentIndexShop1 =
+          (currentIndexShop1 - 1 + shopSlider1.length) % shopSlider1.length;
+        updateShop1();
+      }
+    }
+
+    function updateShop1() {
+      const offset = -currentIndexShop1 * 536; // Adjust this value as needed
+      shopImage1.style.transform = `translateX(${offset}px)`;
+    }
+    shopPrev1.addEventListener("click", prevShop1);
+    shopNext1.addEventListener("click", nextShop1);
+  }
 }
 
 customElements.define('footer-member-component', FooterMember);
---
+++ src/script/resFooter.js
@@ -0,0 +1,122 @@
+const resFooter = () => {
+    return `
+  <style>
+  .accordion-content {
+    transition: max-height 0.4s ease-out, padding 0.4s ease;
+    max-height: 0;
+  }
+</style>
+<footer>
+  <div class="transition">
+    <!-- header -->
+    <div
+      class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-12">
+      <h3>Featured</h3>
+    </div>
+    <!-- Content -->
+    <div class="accordion-content px-5 pt-0 overflow-hidden max-h-0">
+      <ul class="leading-8 text-[#616161] font-light pl-9">
+        <p class="hover:text-black">Air Force 1</p>
+        <p class="hover:text-black">Jordan 1</p>
+        <p class="hover:text-black">React Runner</p>
+        <p class="hover:text-black">Zoom Fly</p>
+        <p class="hover:text-black">VaporMax</p>
+        <p class="hover:text-black">Joyride</p>
+        <p class="hover:text-black">Flyknit Racer</p>
+        <p class="hover:text-black">Zoom Freak</p>
+      </ul>
+    </div>
+  </div>
+  
+  <div class="transition">
+    <!-- header -->
+    <div
+      class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-12">
+      <h3>Shoes</h3>
+    </div>
+    <!-- Content -->
+    <div class="accordion-content px-5 pt-0 overflow-hidden max-h-0">
+      <ul class="leading-8 text-[#616161] font-light pl-9">
+        <p class="hover:text-black">Air Force 1</p>
+        <p class="hover:text-black">Jordan 1</p>
+        <p class="hover:text-black">Kyrie Flytrap</p>
+        <p class="hover:text-black">Zoom Fly</p>
+        <p class="hover:text-black">VaporMax</p>
+        <p class="hover:text-black">Joyride</p>
+        <p class="hover:text-black">Flyknit Racer</p>
+        <p class="hover:text-black">SB Dunk</p>
+      </ul>
+    </div>
+  </div>
+  
+  <div class="transition">
+    <!-- header -->
+    <div
+      class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-12">
+      <h3>Clothing</h3>
+    </div>
+    <!-- Content -->
+    <div class="accordion-content px-5 pt-0 overflow-hidden max-h-0">
+      <ul class="leading-8 text-[#616161] font-light pl-9">
+        <p class="hover:text-black">Sportswear Tech</p>
+        <p class="hover:text-black">Pro Compression</p>
+        <p class="hover:text-black">Dry Training</p>
+        <p class="hover:text-black">Yoga Essential</p>
+        <p class="hover:text-black">Pro Hyperwarm</p>
+        <p class="hover:text-black">Epic Luxe</p>
+        <p class="hover:text-black">Pro Compression</p>
+        <p class="hover:text-black">Dry Training</p>
+        <p class="hover:text-black">Yoga Essential</p>
+      </ul>
+    </div>
+  </div>
+  
+  <div class="transition">
+    <!-- header -->
+    <div
+      class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-12">
+      <h3>Kids</h3>
+    </div>
+    <!-- Content -->
+    <div class="accordion-content px-5 pt-0 overflow-hidden mb-4 max-h-0">
+      <ul class="leading-8 text-[#616161] font-light pl-9">
+        <p class="hover:text-black">Star Runner</p>
+        <p class="hover:text-black">Team Hustle</p>
+        <p class="hover:text-black">Court Borough</p>
+        <p class="hover:text-black">Flex Runner</p>
+        <p class="hover:text-black">Revolution Flex</p>
+        <p class="hover:text-black">Sunray Protect</p>
+        <p class="hover:text-black">Pico</p>
+        <p class="hover:text-black">Downshifter</p>
+      </ul>
+    </div>
+  </div>
+</footer>
+
+  `
+};
+
+class Resfooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = resFooter();
+        const accordionHeader = document.querySelectorAll(".accordion-header");
+        accordionHeader.forEach((header) => {
+            header.addEventListener("click", function () {
+                const accordionContent =
+                    header.parentElement.querySelector(".accordion-content");
+                let accordionMaxHeight = accordionContent.style.maxHeight;
+
+                // Condition handling
+                if (accordionMaxHeight == "0px" || accordionMaxHeight.length == 0) {
+                    accordionContent.style.maxHeight = `${accordionContent.scrollHeight + 32
+                        }px`; // Fix interpolation
+                } else {
+                    accordionContent.style.maxHeight = "0px"; // Add quotes
+                }
+            });
+        });
+    }
+}
+
+customElements.define("res-footer-component", resFooter);
+
