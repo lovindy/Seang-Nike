416e5e894ea838b5f29ad53b7e44f7aa5de1be39
lovindy <seangdy085@gmail.com>
2024-05-23 13:24:29 +0700

    clear comments

---
+++ src/script/layout.js
@@ -0,0 +1,16 @@
+import "./banner.js";
+import "./footerTwin.js";
+import "./navbar.js";
+import "./resFooter.js";
+import "./carousel.js";
+import "./header.js";
+// import "./find.js";
+// import "./footerButtom.js";
+// import "./heroSection.js";
+// import "./feature.js";
+// import "./btn.js";
+// import "./connectThrough.js";
+// import "./membership.js";
+// import "./featuredesign.js"
+// import "./autosilder.js"
+// import "./footerAccordion.js";
