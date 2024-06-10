b7a507a0e400407090b88b356af75d65d1e0e874
lovindy <seangdy085@gmail.com>
2024-06-10 18:40:34 +0700

    modify file .

--- src/script/footerTwin.js
+++ src/script/footerTwin.js
@@ -1,149 +1,197 @@
+const footerData = {
+  upperFooter: [
+    {
+      title: "Featured",
+      items: ["Air Force 1", "Jordan 1", "Metcon", "Air Max 270"],
+    },
+    {
+      title: "Shoes",
+      items: ["All Shoes", "Jordan Shoes", "Basketball Shoes", "Tennis Shoes"],
+    },
+    {
+      title: "Clothing",
+      items: [
+        "All Clothing",
+        "Tops & T-Shirts",
+        "Hoodies & Pullovers",
+        "Sports Bras",
+      ],
+    },
+    {
+      title: "Kids",
+      items: [
+        "Infant & Toddler Shoes",
+        "Kids Shoes",
+        "Kids Basketball Shoes",
+        "Kids Backpacks",
+      ],
+    },
+  ],
+  bottomFooter: [
+    {
+      title: "RESOURCES",
+      items: [
+        "GIFT CARDS",
+        "FIND A STORE",
+        "BECOME A MEMBER",
+        "NIKE X NBA",
+        "NIKE JOURNAL",
+        "Site Feedback",
+      ],
+    },
+    {
+      title: "HELP",
+      items: [
+        "Get Help",
+        "Order Status",
+        "Shipping and Delivery",
+        "Returns",
+        "Order Cancellation",
+        "Payment Options",
+        "Gift Card Balance",
+        "Contact Us",
+      ],
+    },
+    {
+      title: "COMPANY",
+      items: [
+        "About Nike",
+        "News",
+        "Careers",
+        "Investors",
+        "Purpose",
+        "Sustainability",
+      ],
+    },
+    {
+      title: "PROMOTIONS & DISCOUNTS",
+      items: [
+        "Student",
+        "Military",
+        "Teacher",
+        "First Responders & Medical",
+        "Professionals",
+        "Birthday",
+      ],
+    },
+    {
+      title: null,
+      items: [
+        "../images/home-icon/twitter.svg",
+        "../images/home-icon/facebook.svg",
+        "../images/home-icon/youtube.svg",
+        "../images/home-icon/instagram.svg",
+      ],
+      isIcon: true,
+    },
+  ],
+  subFooter: [
+    {
+      items: [
+        "Guides",
+        "Terms of Sale",
+        "Terms of Use",
+        "Nike Privacy Policy",
+        "Your Privacy Choices",
+      ],
+    },
+    {
+      country: "United States",
+      message: "© 2024 Nike, Inc. All Rights Reserved",
+      supplyChain: "CA Supply Chains Act",
+    },
+  ],
+};
+
 const footerTwin = () => {
+  const generateList = (section) => {
+    return `
+      <ul class="flex flex-col leading-8 lg:pt-0 pt-4">
+        <h1 class="pb-2">${section.title}</h1>
+        ${section.items
+          .map(
+            (item) => `<p class="text-[#747476] hover:text-black">${item}</p>`
+          )
+          .join("")}
+      </ul>
+    `;
+  };
+
+  const generateBottomList = (section) => {
+    if (section.isIcon) {
+      return `
+        <ul class="flex gap-2 lg:pt-0 pt-6">
+          ${section.items
+            .map(
+              (src) =>
+                `<a href="#"><img class="w-[30px] rounded-full hover:bg-white bg-gray-300" src="${src}" alt="" /></a>`
+            )
+            .join("")}
+        </ul>
+      `;
+    } else {
+      return `
+        <ul class="leading-8 text-[12px] text-[#757575] md:pt-0 pt-4">
+          <h1 class="text-[14px] text-white">${section.title}</h1>
+          ${section.items
+            .map((item) => `<p class="hover:text-white">${item}</p>`)
+            .join("")}
+        </ul>
+      `;
+    }
+  };
+
   return `
-  <!-- Upper footer -->
-<footer class="w-full px-6 sm:py-16">
-  <div
-    class="max-w-screen-xl hidden sm:grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 mx-auto  ">
-    <ul class="flex flex-col leading-8 lg:pt-0 pt-4">
-      <h1 class="pb-2">Featured</h1>
-      <p class="text-[#747476] hover:text-black">Air Force 1</p>
-      <p class="text-[#747476] hover:text-black">Jordan 1</p>
-      <p class="text-[#747476] hover:text-black">Metcon</p>
-      <p class="text-[#747476] hover:text-black">Air Max 270</p>
-    </ul>
+    <!-- Upper footer -->
+    <footer class="w-full px-6 sm:py-16">
+      <div class="max-w-screen-xl hidden sm:grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 mx-auto">
+        ${footerData.upperFooter
+          .map((section) => generateList(section))
+          .join("")}
+      </div>
+    </footer>
 
-    <ul class="flex flex-col leading-8 lg:pt-0 pt-4">
-      <h1 class="pb-2">Shoes</h1>
-      <p class="text-[#747476] hover:text-black">All Shoes</p>
-      <p class="text-[#747476] hover:text-black">Jordan Shoes</p>
-      <p class="text-[#747476] hover:text-black">Basketball Shoes</p>
-      <p class="text-[#747476] hover:text-black">Tennis Shoes</p>
-    </ul>
+    <!-- Bottom footer -->
+    <footer class="bg-black w-full">
+      <div class="max-w-[1600px] mx-auto p-6 grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-2">
+        ${footerData.bottomFooter
+          .map((section) => generateBottomList(section))
+          .join("")}
+      </div>
 
-    <ul class="flex flex-col leading-8 lg:pt-0 pt-4">
-      <h1 class="pb-2">Clothing</h1>
-      <p class="text-[#747476] hover:text-black">All Clothing</p>
-      <p class="text-[#747476] hover:text-black">Tops & T-Shirts</p>
-      <p class="text-[#747476] hover:text-black">Hoodies & Pullovers</p>
-      <p class="text-[#747476] hover:text-black">Sports Bras</p>
-    </ul>
-
-    <ul class="flex flex-col leading-8 lg:pt-0 pt-4">
-      <h1 class="pb-2">Kids</h1>
-      <p class="text-[#747476] hover:text-black">Infant & Toddler Shoes</p>
-      <p class="text-[#747476] hover:text-black">Kids Shoes</p>
-      <p class="text-[#747476] hover:text-black">Kids Basketball Shoes</p>
-      <p class="text-[#747476] hover:text-black">Kids Backpacks</p>
-    </ul>
-  </div>
-</footer>
-
-<!-- bottom footer -->
-<footer class="bg-black w-full">
-  <div class="max-w-[1600px] mx-auto p-6 grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-2">
-    <ul class="font-medium text-[14px] text-white leading-8 md:pt-0 pt-4">
-      <h1 class="cursor-pointer">RESOURCES</h1>
-      <h1 class="cursor-pointer">GIFT CARDS</h1>
-      <h1 class="cursor-pointer">FIND A STORE</h1>
-      <h1 class="cursor-pointer">BECOME A MEMBER</h1>
-      <h1 class="cursor-pointer">NIKE X NBA</h1>
-      <h1 class="cursor-pointer">NIKE JOURNAL</h1>
-      <h1 class="cursor-pointer">Site Feedback</h1>
-    </ul>
-
-    <ul class="leading-8 text-[12px] text-[#757575] md:pt-0 pt-4">
-      <h1 class="text-[14px] text-white">HELP</h1>
-      <p class="hover:text-white">Get Help</p>
-      <p class="hover:text-white">Order Status</p>
-      <p class="hover:text-white">Shipping and Delivery</p>
-      <p class="hover:text-white">Returns</p>
-      <p class="hover:text-white">Order Cancellation</p>
-      <p class="hover:text-white">Payment Options</p>
-      <p class="hover:text-white">Gift Card Balance</p>
-      <p class="hover:text-white">Contact Us</p>
-    </ul>
-
-    <ul class="leading-8 text-[12px] text-[#757575] md:pt-0 pt-4">
-      <h1 class="text-[14px] text-white">COMPANY</h1>
-      <p class="hover:text-white">About Nike</p>
-      <p class="hover:text-white">News</p>
-      <p class="hover:text-white">Careers</p>
-      <p class="hover:text-white">Investors</p>
-      <p class="hover:text-white">Order Cancellation</p>
-      <p class="hover:text-white">Purpose</p>
-      <p class="hover:text-white">Sustainability</p>
-    </ul>
-
-    <ul class="leading-8 text-[12px] text-[#757575] md:pt-0 pt-4">
-      <h1 class="text-[14px] text-white">PROMOTIONS & DISCOUNTS</h1>
-      <p class="hover:text-white">Student</p>
-      <p class="hover:text-white">Military</p>
-      <p class="hover:text-white">Teacher</p>
-      <p class="hover:text-white">First Responders & Medical</p>
-      <p class="hover:text-white">Professionals</p>
-      <p class="hover:text-white">Birthday</p>
-    </ul>
-
-    <ul class="flex gap-2 lg:pt-0 pt-6">
-      <a href="#"
-        ><img
-          class="w-[30px] rounded-full hover:bg-white bg-gray-300"
-          src="../images/home-icon/twitter.svg"
-          alt=""
-      /></a>
-      <a href="#"
-        ><img
-          class="w-[30px] rounded-full hover:bg-white bg-gray-300"
-          src="../images/home-icon/facebook.svg"
-          alt=""
-      /></a>
-      <a href="#"
-        ><img
-          class="w-[30px] rounded-full hover:bg-white bg-gray-300"
-          src="../images/home-icon/youtube.svg"
-          alt=""
-      /></a>
-      <a href="#"
-        ><img
-          class="w-[30px] rounded-full hover:bg-white bg-gray-300"
-          src="../images/home-icon/instagram.svg"
-          alt=""
-      /></a>
-    </ul>
-  </div>
-
-  <!-- sub-footer -->
-  <div
-    class="max-w-[1600px] mx-auto p-10 flex flex-col justify-between">
-    <div class="w-full">
-        <ul class="gap-6 flex justify-end text-[#757575] text-[12px]">
-            <p class="hover:text-white">Guides</p>
-            <p class="hover:text-white">Terms of Sale</p>
-            <p class="hover:text-white">Terms of Use</p>
-            <p class="hover:text-white">Nike Privacy Policy</p>
-            <p class="hover:text-white">Your Privacy ChoicesYour Privacy Choices</p>
+      <!-- Sub-footer -->
+      <div class="max-w-[1600px] mx-auto p-10 flex flex-col justify-between">
+        <div class="w-full">
+          <ul class="gap-6 flex justify-end text-[#757575] text-[12px]">
+            ${footerData.subFooter[0].items
+              .map((item) => `<p class="hover:text-white">${item}</p>`)
+              .join("")}
           </ul>
-    </div>
+        </div>
 
-    <div class="flex justify-between items-center">
-      <span class="flex items-center gap-4"
-        ><img class="cursor-pointer w-[20px]" src="../images/home-icon/location.svg" alt="" />
-        <p class="text-white cursor-pointer text-[12px]">United States</p>
-        <p class="text-[#757575] text-[12px]">
-          © 2024 Nike, Inc. All Rights Reserved
-        </p></span
-      >
-      <p class="text-[12px] text-[#757575] hover:text-white">CA Supply Chains Act</p>
-    </div>
-  </div>
-</footer>
-  `
-}
+        <div class="flex justify-between items-center">
+          <span class="flex items-center gap-4">
+            <img class="cursor-pointer w-[20px]" src="../images/home-icon/location.svg" alt="" />
+            <p class="text-white cursor-pointer text-[12px]">${
+              footerData.subFooter[1].country
+            }</p>
+            <p class="text-[#757575] text-[12px]">${
+              footerData.subFooter[1].message
+            }</p>
+          </span>
+          <p class="text-[12px] text-[#757575] hover:text-white">${
+            footerData.subFooter[1].supplyChain
+          }</p>
+        </div>
+      </div>
+    </footer>
+  `;
+};
 
 class FooterTwin extends HTMLElement {
   connectedCallback() {
     this.innerHTML = footerTwin();
   }
 }
-customElements.define("footer-twin", FooterTwin);
 
+customElements.define("footer-twin", FooterTwin);
